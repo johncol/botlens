dad69549a112c3fa3e0450758a08cd9d16e513de
John Cely <john.19col@gmail.com>
2026-07-21 17:22:46 -0700

    Rename middleware to proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { verifyToken, AUTH_COOKIE } from "@/lib/auth";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   // Always allow the login page and its API route through
